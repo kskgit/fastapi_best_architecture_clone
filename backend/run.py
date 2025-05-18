cf062e5c2efcfee998cff1ed6ca6fe6f636d7dea
nakao keisuke <r3grashopper193@gmail.com>
2025-05-18 11:37:36 +0900

    サーバがリスタート可能なように変更

--- backend/run.py
+++ backend/run.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     # 如果你喜欢在 IDE 中进行 DEBUG，可在 IDE 中直接右键启动此文件
     # 如果你喜欢通过 print 方式进行调试，建议使用 fastapi cli 方式启动服务
     try:
-        config = uvicorn.Config(app='backend.main:app', reload=True)
+        config = uvicorn.Config(app='backend.main:app', host='0.0.0.0', port=8000, reload=True)
         server = uvicorn.Server(config)
         server.run()
     except Exception as e:
